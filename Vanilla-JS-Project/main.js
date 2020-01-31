1cd2138575318e5aa6161f8fb7d7c4d76be76488
Ramona Saintandre <thenewmona@gmail.com>
2020-01-31 17:36:41 -0500

    added notes.md and a gitignore file

--- Vanilla-JS-Project/main.js
+++ Vanilla-JS-Project/main.js
@@ -4,5 +4,6 @@ let textAdress = document.getElementById('address');
 
 btnGetData.oncick = function(){
     let address=textAdress.node ;
+    // civic information api - need to get a key 
     var url = "https://www.googleapis.com/civicinfo/v2/representatives?key=" + googleKey + "&address="
 + addressconsole.log(url);}
